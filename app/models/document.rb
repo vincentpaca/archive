af4eb80e8e17e16a737a461f1785ab15c3190d93
Vincent Paca <vincent.paca@gmail.com>
2012-11-25 12:30:50 +0800

    Added file attachment to documents

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,10 @@
 class Document < ActiveRecord::Base
-  attr_accessible :content, :file_name, :title
+  attr_accessible :content, :file_name, :title, :file
+
+  has_attached_file :file
+
+  validates_attachment_content_type :pdf,
+    :content_type => [ 'application/pdf' ],
+    :message => "Only PDF files are supported."
+
 end
