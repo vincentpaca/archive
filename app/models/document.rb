d28c5d345965782c6e0be763af951af19313b5b8
Vincent Paca <vincent.paca@gmail.com>
2012-11-25 23:04:01 +0800

    Added logic for processing documents

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,18 +2,13 @@ class Document < ActiveRecord::Base
   attr_accessible :file_name, :title, :file
 
   has_attached_file :file
-
-  validates_attachment_content_type :pdf,
-    :content_type => [ 'application/pdf' ],
-    :message => "Only PDF files are supported."
-
   has_many :pages
-
+  
   after_create :process_document
 
   private
 
   def process_document
-    DocumentWorker.perform_async(self)
+    DocumentWorker.perform_async(self, self.file)
   end
 end
--- app/workers/document_worker.rb
+++ app/workers/document_worker.rb
@@ -1,11 +1,11 @@
 class DocumentWorker
   include Sidekiq::Worker
 
-  def perform(doc)
-    reader = PDF::Reader.new(doc.file)
+  def perform(doc, file)
+    reader = PDF::Reader.new(file)
 
     reader.pages.each_with_index do |page, index|
-      p = Page.new(:document => doc, :number => index, :content => page.text)
+      p = Page.new(:document_id => doc, :number => index, :content => page.text)
       p.save
     end
   end
